e1b82de4a407d4badf8c864841d4e0336daa23ee
gtpepi <gtpepi@hotmail.com>
2022-05-17 21:39:13 -0300

    Update position and sizes

--- src/index.js
+++ src/index.js
@@ -12,18 +12,23 @@ const messages = document.getElementById("messages");
 const bookmarks = document.getElementById("bookmarks");
 
 const homeIcon = document.createElement("button");
+homeIcon.setAttribute("class", "menuBtn");
 homeIcon.innerHTML = homeSvg;
 
 const exploreIcon = document.createElement("button");
+exploreIcon.setAttribute("class", "menuBtn");
 exploreIcon.innerHTML = exploreSvg;
 
 const notificationsIcon = document.createElement("button");
+notificationsIcon.setAttribute("class", "menuBtn");
 notificationsIcon.innerHTML = notificationsSvg;
 
 const messagesIcon = document.createElement("button");
+messagesIcon.setAttribute("class", "menuBtn");
 messagesIcon.innerHTML = messagesSvg;
 
 const bookmarksIcon = document.createElement("button");
+bookmarksIcon.setAttribute("class", "menuBtn");
 bookmarksIcon.innerHTML = bookmarksSvg;
 
 //appends
